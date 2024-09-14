36558cf72f23eee7d2647c4baf0bbdabef030ae2
Melissa Harrison <78873771+melissaahh@users.noreply.github.com>
2024-09-14 00:23:41 -0400

    Added overtime_pay

--- build-an-hours-worked-function/main.py
+++ build-an-hours-worked-function/main.py
@@ -1,5 +1,10 @@
 # Write function here
 def calculate_pay(hours_worked, pay_per_hour):
+    if hours_worked > 40:
+        overtime_hours = hours_worked -40
+        regular_pay = 40 * pay_per_hour
+        overtime_pay = overtime_hours * pay_per_hour * 2
+        return overtime_pay + regular_pay
     return hours_worked * pay_per_hour
 # Worked 40 hours at $20 an hour
 print(calculate_pay(40,20))
