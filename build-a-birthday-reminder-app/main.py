4154112723e14c4870272cbecd48ac4f077aa8f5
Melissa Harrison <78873771+melissaahh@users.noreply.github.com>
2024-09-13 22:35:04 -0400

    Added if statements

--- build-a-birthday-reminder-app/main.py
+++ build-a-birthday-reminder-app/main.py
@@ -1,1 +1,10 @@
 user_input = input("Which birthday are you looking for? ")
+
+if user_input == "Dylan":
+    print("Birthday is 07/02/89!")
+elif user_input == "Melisa":
+    print("Birthday is 05/03/89!")
+elif user_input == "Elizabeth":
+    print("Birthday is 06/01/2025")
+else:
+    print("Can't find a birthday for this person...")
