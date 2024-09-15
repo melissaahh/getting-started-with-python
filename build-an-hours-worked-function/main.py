9d9ffa92710ca63ada634b4f80b2ec64aca11862
Melissa Harrison <78873771+melissaahh@users.noreply.github.com>
2024-09-15 00:13:40 -0400

    Added new function

--- build-an-hours-worked-function/main.py
+++ build-an-hours-worked-function/main.py
@@ -7,8 +7,17 @@ def calculate_pay(hours_worked, pay_per_hour):
         return overtime_pay + regular_pay
     return hours_worked * pay_per_hour
 # Worked 40 hours at $20 an hour
-print(calculate_pay(40,20))
+#print(calculate_pay(40,20))
 # Worked 50 hours at $20 an hour
-print(calculate_pay(50,20))
+#print(calculate_pay(50,20))
 # Worked 40 hours at $12 an hour
-print(calculate_pay(40,12))
+#print(calculate_pay(40,12))
+
+def calculate_monthly_pay(wk_1_hours, wk_2_hours, wk_3_hours, wk_4_hours, pay_per_hour):
+    week1_pay = calculate_pay(wk_1_hours, pay_per_hour)
+    week2_pay = calculate_pay(wk_2_hours, pay_per_hour)
+    week3_pay = calculate_pay(wk_3_hours, pay_per_hour)
+    week4_pay = calculate_pay(wk_4_hours, pay_per_hour)
+    return week1_pay + week2_pay + week3_pay + week4_pay
+
+print(calculate_monthly_pay(40,50,35,40,50))
