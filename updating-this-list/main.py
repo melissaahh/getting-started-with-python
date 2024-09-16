da9010fabe2a70e3539361ba256976b78e2f4770
Melissa Harrison <78873771+melissaahh@users.noreply.github.com>
2024-09-16 13:03:34 -0400

    Added list

---
+++ updating-this-list/main.py
@@ -0,0 +1,30 @@
+names = ["Dylan",
+         "Karen",
+         "Richie",
+         "Ray",
+         "Olga",
+         "Rachel",
+         "Carsten",
+         "Drew",
+         "Lorraine",
+         "Braydon",
+         "Ashton",
+         "Alysia",
+         "Shaun",
+         "Lisa",
+         "Stephen",
+         "Nelia",
+         "David",
+         "Makayla",
+         "Jordan",
+         "Julian"]
+
+dinner_invite = names[:4]
+drinks = names[16:]
+hike = names[10:14]
+morning_walk = names[0]
+
+print(dinner_invite)
+print(drinks)
+print(hike)
+print(morning_walk)
