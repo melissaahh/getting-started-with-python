4febd184a5dc2e6250988a4950413f23e8a3b9aa
Melissa Harrison <78873771+melissaahh@users.noreply.github.com>
2024-09-16 12:41:26 -0400

    Changed list elements

--- clean-up-this-list/main.py
+++ clean-up-this-list/main.py
@@ -1,1 +1,7 @@
 websites = ["www.google.com","www.youtube.com","www.dropbox.com","www.quora.com","www.reddit.com","www.instagram.com"]
+
+websites.pop(2)
+websites.insert(2,"www.yahoo.com")
+websites.append("www.hotmail.com")
+
+print(websites)
