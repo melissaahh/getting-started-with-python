2d123fbfac856d8125fd73c0dc12b4521e210c0a
Melissa Harrison <78873771+melissaahh@users.noreply.github.com>
2024-09-16 15:37:42 -0400

    Added while loop

--- build-a-language-translator/main.py
+++ build-a-language-translator/main.py
@@ -7,3 +7,14 @@ translations = {
 done = False
 
 print('Type "done" at any time to exit')
+
+while not done:
+  word = input("Type an English word to translate: ")
+  word = word.lower()
+
+  if word == "done":
+   done = True
+  elif word in translations:
+    print(translations[word])
+  else:
+    print("Translation not known")
