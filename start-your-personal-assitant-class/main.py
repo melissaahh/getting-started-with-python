c5388f0d1f88eac3c46f99131f0dc8e6f50b05b4
Melissa Harrison <78873771+melissaahh@users.noreply.github.com>
2024-09-27 18:20:13 -0400

    Created a class

--- start-your-personal-assitant-class/main.py
+++ start-your-personal-assitant-class/main.py
@@ -1,10 +1,15 @@
 # Give the class a name
-class:
+class PersonalAssistant:
   # Add an __init__ function here
-
+  def __init__(self):
   # Complete the get_contact function code
+    self.contacts = {'Ann': 'Marketing Coordinator', 'Chelsea': 'Software Developer', 'Nichole': 'Sales Representative', 'Max': 'Technical Writer'}
+    self.todos = []
   def get_contact(self, name):
-
+    if name in self.contacts:
+      return self.contacts[name]
+    else:
+      return "No contact with that name"
 
 # Code to test output of the class
 assistant = PersonalAssistant()
